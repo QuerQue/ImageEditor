cff1e9eec90ed64e357e3631cfc9a49cc2c4ef67
ntomeky <ntomeky@gmail.com>
2017-03-09 15:39:45 +0100

    negative

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -21,13 +21,15 @@ public class Controller {
     @FXML
     ImageView imageSpace;
 
+    BufferedImage bufferedImage;
+
     @FXML
     private void openButtonClick(){
         FileChooser fc = new FileChooser();
         File selectedFile = fc.showOpenDialog(null);
 
         try {
-            BufferedImage bufferedImage = ImageIO.read(selectedFile);
+            bufferedImage = ImageIO.read(selectedFile);
             Image image = SwingFXUtils.toFXImage(bufferedImage, null);
             imageSpace.setImage(image);
         }catch(IOException ex){
@@ -35,6 +37,77 @@ public class Controller {
         }
     }
 
+    @FXML
+    private void rotateButton45(){
+        imageSpace.setRotate(45);
+    }
 
+    @FXML
+    private void rotateButton90(){
+        imageSpace.setRotate(90);
+    }
+
+    @FXML
+    private void rotateButton135(){
+        imageSpace.setRotate(135);
+    }
+
+    @FXML
+    private void rotateButton180(){
+        imageSpace.setRotate(180);
+    }
+
+    @FXML
+    private void rotateButton225(){
+        imageSpace.setRotate(225);
+    }
+
+    @FXML
+    private void rotateButton270(){
+        imageSpace.setRotate(270);
+    }
+
+    @FXML
+    private void rotateButton315(){
+        imageSpace.setRotate(315);
+    }
+
+    @FXML
+    private void rotateButton360(){
+        imageSpace.setRotate(360);
+    }
+
+
+    @FXML
+    private void toNegative() {
+        int width = bufferedImage.getWidth();
+        int height = bufferedImage.getHeight();
+
+        for(int y=0; y<height; y++){
+            for(int x=0; x<width; x++){
+                int p = bufferedImage.getRGB(x,y);
+                int a = (p>>24)&0xff;
+                int r = (p>>16)&0xff;
+                int g = (p>>8)&0xff;
+                int b = p&0xff;
+
+                r = 255 - r;
+                g = 255 - g;
+                b = 255 - b;
+
+                p = (a<<24) | (r<<16) | (g<<8) | b;
+                bufferedImage.setRGB(x, y, p);
+
+            }
+        }
+        Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+        imageSpace.setImage(image);
+
+    }
+
+    @FXML
+    private void blackAndWhite(){
+
+    }
 
 }
--- src/sample/Model.java
+++ src/sample/Model.java
@@ -1,9 +1,17 @@
 package sample;
 
+
+import javafx.scene.image.Image;
+
+
+
+
+
 /**
  * Created by Tomek on 2/23/2017.
  */
 public class Model
 {
-    
+
+
 }
